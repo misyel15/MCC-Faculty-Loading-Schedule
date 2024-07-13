bd3a5a812700886d5a265383666387aa0d1db7fd
Faculty Loading <cosemichelle2@gmail.com>
2024-07-13 11:39:03 +0800

    Update db_connect.php

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,3 +1,3 @@
 <?php 
-
-$conn= new mysqli('127.0.0.1','u510162695_scheduling_db','','u510162695_scheduling_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn = new mysqli('127.0.0.1', 'u510162695_scheduling_db', '', 'u510162695_scheduling_db', 3306) or die("Could not connect to mysql: " . mysqli_error($conn));
+?>
