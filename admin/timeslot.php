78742cdc609384592a5590349b907b909b38fceb
Faculty Loading <cosemichelle2@gmail.com>
2024-07-19 15:28:18 +0800

    Update timeslot.php

--- admin/timeslot.php
+++ admin/timeslot.php
@@ -48,7 +48,7 @@
 							</div>
 							<div class="modal-footer">
 							<button type="button" class="btn btn-primary" id="saveTimeslotBtn">Save</button>
-							<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+							<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
 						</div>
 						</div>
 					</div>
