da692d7a05adb0f972d2b5eff57a561e74394dfd
Faculty Loading <cosemichelle2@gmail.com>
2024-07-19 15:20:34 +0800

    Update section.php

--- admin/section.php
+++ admin/section.php
@@ -54,7 +54,7 @@
 						</div>
 						<div class="modal-footer">
 							<button type="button" class="btn btn-primary" id="saveSectionBtn">Save</button>
-							<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+							<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
 
 						</div>
 					</div>
