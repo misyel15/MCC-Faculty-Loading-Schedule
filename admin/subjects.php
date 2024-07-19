0aa3e8afbf8e21d939568bb41db1bdead3ca66a4
Faculty Loading <cosemichelle2@gmail.com>
2024-07-19 15:24:51 +0800

    Update subjects.php

--- admin/subjects.php
+++ admin/subjects.php
@@ -189,7 +189,7 @@
                             </div>
                             <div class="modal-footer">
                                 <button type="submit" class="btn btn-primary">Save</button>
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
                             </div>
                         </form>
                     </div>
